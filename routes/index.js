98a10f8a8fe343933820867268147120d67c0fe9
David Rosa <rosatamsen@gmail.com>
2024-04-09 19:19:47 -0600

    app: Rename project name strings before test deployment.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Flixmate' });
 });
 
 // Google OAuth login route
